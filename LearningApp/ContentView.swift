96678282ddd969f273abd2608a4cfe670660b9c5
Tzortzis Kapellas <tzortziskap@hotmail.com>
2021-08-14 01:10:17 +0300

    Initial Commit

---
+++ LearningApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by Tzortzis Kapellas on 14/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Tzortzis Kapellas on 14/8/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
